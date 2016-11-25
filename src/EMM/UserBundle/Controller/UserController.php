4cf2e1055e56765cfde7c77bda6dd19e80f3790d
Herberth Lucho <h3ealh@gmail.com>
2016-11-25 21:28:44 +0000

    second commit

--- src/EMM/UserBundle/Controller/UserController.php
+++ src/EMM/UserBundle/Controller/UserController.php
@@ -9,8 +9,39 @@ class UserController extends Controller
 {
     public function indexAction()
     {
-        return new Response('Bienvenido al modulo de usuario');
+        $em = $this->getDoctrine()->getManager();
+        
+        $users = $em->getRepository('EMMUserBundle:User')->findAll();
+        
+        /*
+        $res = 'Lista de usuarios: <br />';
+        
+        foreach($users as $user)
+        {
+            $res .= 'Usuario: ' . $user->getUsername() . ' - Email: ' . $user->getEmail() . '<br />';
+        }
+        
+        return new Response($res);
+        */
+        
+        return $this->render('EMMUserBundle:User:index.html.twig', array('users' => $users));
     }
+    
+    public function viewAction($id)
+    {
+        $repository = $this->getDoctrine()->getRepository('EMMUserBundle:User');
+        
+        $user = $repository->find($id);
+        
+        //$user = $repository->findOneByUsername($nombre);
+        
+        return new Response('Usuario: ' . $user->getUsername() . ' con email: ' . $user->getEmail());
+    }
+    
+    
+    
+    
+    
     
     public function articlesAction($page)
     {
